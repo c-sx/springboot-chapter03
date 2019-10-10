4a32b21625a4a03451fc9c1be9bca3d6e847d4cc
csx <csx17@qq.com>
2019-10-10 19:38:15 +0800

    Initial Program

---
+++ src/main/java/com/mpocket/springbootchapter03/SpringbootChapter03Application.java
@@ -0,0 +1,13 @@
+package com.mpocket.springbootchapter03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChapter03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootChapter03Application.class, args);
+    }
+
+}
